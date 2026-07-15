355ef8de6ae860554501c9895b28fa41db64299b
jalmavani027-alt <jalmavani027@gmail.com>
2026-07-15 16:51:19 +0530

    Add files via upload
    Basic introduction to Variables

---
+++ intro to variable.py
@@ -0,0 +1,22 @@
+a= 1
+b= 6
+print (a+b)
+
+
+
+# rules for variables
+# 1. Variables must start with a letter or underscore
+# 2. Variables cannot start with a number
+# 3. Variables can only contain alphanumeric characters and underscores
+# 4. Variables are case-sensitive
+# 5. Variables cannot be reserved keywords
+
+
+a = 100
+b = float(a)
+
+c= type(b)
+print(c)
+
+
+
