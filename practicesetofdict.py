b4b7bf6a9b4f9c4d67665bda334365026b076e2b
jalmavani027-alt <jalmavani027@gmail.com>
2026-07-18 13:06:03 +0530

    Add files via upload
    Practice set of Dictionary & Sets

--- practicesetofdict.py
+++ practicesetofdict.py
@@ -1,62 +1,62 @@
-# # This is practice set of Dictionary & Sets
+# This is practice set of Dictionary & Sets
 
-# # Question 1 : Write a program to create a dictionary of Hindi words with values as their English
-# # translation. Provide user with an option to look it up!
+# Question 1 : Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
 
-# translate = {
-#     "dekh" : "see",
-#     "khursi" : "chair",
-#     "madad" : "help",
-#     "aadmi" : "guy",
-#     "bakri" : "goat"
-# }
+translate = {
+    "dekh" : "see",
+    "khursi" : "chair",
+    "madad" : "help",
+    "aadmi" : "guy",
+    "bakri" : "goat"
+}
 
-# a = input("Write dowm hindi word here you get english meaning of that words :")
-# print(translate[a])
-
-
-# # Question 2 : Write a program to create a dictionary of Hindi words with values as their English
-# # translation. Provide user with an option to look it up!.
-
-# s = set()
-
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-# a = input("Enter number :")
-# s.add(int(a))
-
-# print(s)
+a = input("Write dowm hindi word here you get english meaning of that words :")
+print(translate[a])
 
 
-# # Question 3 :  Can we have a set with 18 (int) and '18' (str) as a value in it?
+# Question 2 : Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!.
 
-# t = set()
-# # t.add(int(18))
-# # t.add("18")
-# # print(t)
+s = set()
+
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+a = input("Enter number :")
+s.add(int(a))
+
+print(s)
+
+
+# Question 3 :  Can we have a set with 18 (int) and '18' (str) as a value in it?
+
+t = set()
+t.add(int(18))
+t.add("18")
+print(t)
 
 
 
-# # Question 4 : What will be the length of following set s:
+# Question 4 : What will be the length of following set s:
 
-# s = set()
-# s.add(20)
-# s.add(20.0)
-# s.add('20') 
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') 
 
-# print(len(s))
+print(len(s))
 
 
 # Question 5 : Create an empty dictionary. Allow 4 friends to enter their favorite language as value and
@@ -85,7 +85,7 @@ print(d)
 
 # Question 6 : . Can you change the values inside a list which is contained in set S?
 
-s = {8, 7, 12, "Harry", [1,2]}
+# s = {8, 7, 12, "Harry", [1,2]}
 
 # we cannot change the value inside the sets it is not possible by any methods 
 # Also you cannot add the list inside the any sets.
